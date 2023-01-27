047a104fc6229e6e86603dfb5c8e5b5d97876993
Giorios <90624324+Giorios@users.noreply.github.com>
2023-01-27 11:55:26 +0000

    added the last pictures and colors and functions

---
+++ script.js
@@ -0,0 +1,28 @@
+function bBut() {
+  document.getElementById('bphoto').src = "img/bbutterfly.jpg" }
+
+function wBut() {
+  document.getElementById('bphoto').src = "img/wbutterfly.jpg" 
+}
+
+  function oBut() {
+  document.getElementById('bphoto').src = "img/obuttefly.jpg" }
+
+function oFro() {
+  document.getElementById('fpic').src = "img/ofrog.jpg" }
+
+function pMan() {
+  document.getElementById('mpic').src = "img/pmantis.jpg" }
+
+function bMan() {
+  document.getElementById('mpic').src = "img/bmantis.jpg" }
+
+function gMan() {
+  document.getElementById('mpic').src = "img/gmantis.jpg" }
+
+function oLad() {
+  document.getElementById('lpic').src = "img/oladybug.jpg" }
+function rLad() {
+  document.getElementById('lpic').src = "img/rladybug.jpg" }
+function yLad() {
+  document.getElementById('lpic').src = "img/yladybug.jpg" }
