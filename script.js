8beab32c2e19fadf52ef8674969628d8a5ee29e2
Giorios <90624324+Giorios@users.noreply.github.com>
2023-01-27 13:32:42 +0000

    completely changed card and button style

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 function bBut() {
-  document.getElementById('bphoto').src = "img/bbutterfly.jpg" }
-
-function wBut() {
-  document.getElementById('bphoto').src = "img/wbutterfly.jpg" 
+  document.getElementById('bphoto').src = "img/bbutterfly.jpg"
 }
 
-  function oBut() {
-  document.getElementById('bphoto').src = "img/obuttefly.jpg" }
+function wBut() {
+  document.getElementById('bphoto').src = "img/wbutterfly.jpg"
+}
+
+function oBut() {
+  document.getElementById('bphoto').src = "img/obuttefly.jpg"
+}
 
 function oFro() {
-  document.getElementById('fpic').src = "img/ofrog.jpg" }
+  document.getElementById('fpic').src = "img/ofrog.jpg"
+}
+
+function tFro() {
+  document.getElementById('fpic').src = "img/treefrog.jpg"
+}
+
+function bFro() {
+  document.getElementById('fpic').src = "img/bfrog.jpg"
+}
+
 
 function pMan() {
-  document.getElementById('mpic').src = "img/pmantis.jpg" }
+  document.getElementById('mpic').src = "img/pmantis.jpg"
+}
 
 function bMan() {
-  document.getElementById('mpic').src = "img/bmantis.jpg" }
+  document.getElementById('mpic').src = "img/bmantis.jpg"
+}
 
 function gMan() {
-  document.getElementById('mpic').src = "img/gmantis.jpg" }
+  document.getElementById('mpic').src = "img/gmantis.jpg"
+}
 
 function oLad() {
-  document.getElementById('lpic').src = "img/oladybug.jpg" }
+  document.getElementById('lpic').src = "img/oladybug.jpg"
+}
 function rLad() {
-  document.getElementById('lpic').src = "img/rladybug.jpg" }
+  document.getElementById('lpic').src = "img/rladybug.jpg"
+}
 function yLad() {
-  document.getElementById('lpic').src = "img/yladybug.jpg" }
+  document.getElementById('lpic').src = "img/yladybug.jpg"
+}
